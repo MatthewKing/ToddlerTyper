e52151d6e972bcbc9a2777abb726217814da8fda
Matthew King <mking@mking.net>
2022-02-01 13:58:20 +0800

    Add text resize hotkeys.

---
+++ src/ToddlerTyper/HotkeyManager.cs
@@ -0,0 +1,36 @@
+﻿namespace ToddlerTyper;
+
+public class HotkeyManager
+{
+    private readonly KeysConverter _keysConverter;
+    private readonly IDictionary<string, Action> _handlers;
+
+    public HotkeyManager()
+    {
+        _keysConverter = new KeysConverter();
+        _handlers = new Dictionary<string, Action>(StringComparer.OrdinalIgnoreCase);
+    }
+
+    public void SetHandler(string hotkey, Action handler)
+    {
+        _handlers[hotkey] = handler;
+    }
+
+    public bool Handle(Keys keyData)
+    {
+        var hotkey = _keysConverter.ConvertToInvariantString(keyData);
+
+        return Handle(hotkey);
+    }
+
+    public bool Handle(string? hotkey)
+    {
+        if (hotkey != null && _handlers.TryGetValue(hotkey, out var handler))
+        {
+            handler.Invoke();
+            return true;
+        }
+
+        return false;
+    }
+}
--- src/ToddlerTyper/ToddlerTyperForm.cs
+++ src/ToddlerTyper/ToddlerTyperForm.cs
@@ -2,8 +2,38 @@ namespace ToddlerTyper;
 
 internal sealed partial class ToddlerTyperForm : Form
 {
+    private const int FontSizeIncrement = 10;
+    private const int FontSizeMin = 10;
+    private const int FontSizeMax = 250;
+
+    private readonly HotkeyManager _hotkeys;
+
     public ToddlerTyperForm()
     {
+        _hotkeys = new HotkeyManager();
+        _hotkeys.SetHandler("Ctrl+Up", () => UpdateFontSize(FontSizeIncrement, FontSizeMin, FontSizeMax));
+        _hotkeys.SetHandler("Ctrl+Down", () => UpdateFontSize(-FontSizeIncrement, FontSizeMin, FontSizeMax));
+
         InitializeComponent();
+    }
+
+    protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+    {
+        if (_hotkeys.Handle(keyData))
+        {
+            return true;
+        }
+
+        return base.ProcessCmdKey(ref msg, keyData);
+    }
+
+    private void UpdateFontSize(int increment, int min, int max)
+    {
+        var newSize = Math.Clamp(uxTextBox.Font.Size + increment, min, max);
+        if (newSize != uxTextBox.Font.Size)
+        {
+            using var font = new Font(uxTextBox.Font.FontFamily, newSize);
+            uxTextBox.Font = font;
+        }
     }
 }
