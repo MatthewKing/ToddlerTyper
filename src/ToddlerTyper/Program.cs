92b9f2524602484016b0728980a5d1cfe3d50368
Matthew King <mking@mking.net>
2022-02-02 10:37:40 +0800

    Expand application configuration. Change DPI mode.

--- src/ToddlerTyper/Program.cs
+++ src/ToddlerTyper/Program.cs
@@ -5,8 +5,10 @@ internal static class Program
     [STAThread]
     static void Main()
     {
-        ApplicationConfiguration.Initialize();
-
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
+        
         Application.Run(new ToddlerTyperApplicationContext());
     }
 }
